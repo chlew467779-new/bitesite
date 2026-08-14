f9d7ce1a417cb745d24fc903ca14a113b3956029
chlew467779-new <chlew467779@gmail.com>
2026-08-14 21:40:24 +0800

    Create index.ts to export layout components

---
+++ app/layouts/index.ts
@@ -0,0 +1,15 @@
+import ClassicLayout from "./classic-layout";
+import ElegantLayout from "./elegant-layout";
+import MinimalLayout from "./minimal-layout";
+import ModernLayout from "./modern-layout";
+import RusticLayout from "./rustic-layout";
+
+export const layouts = {
+  classic: ClassicLayout,
+  elegant: ElegantLayout,
+  minimal: MinimalLayout,
+  modern: ModernLayout,
+  rustic: RusticLayout,
+};
+
+export type LayoutType = keyof typeof layouts;
