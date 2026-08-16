511a7820a77d04a5e95b917a3031eed532d2ab1d
chlew467779-new <chlew467779@gmail.com>
2026-08-16 20:18:42 +0800

    Add BITESITE_WHATSAPP_URL constant

---
+++ lib/whatsapp.ts
@@ -0,0 +1,2 @@
+export const BITESITE_WHATSAPP_URL =
+  "https://wa.me/60165660239?text=Hi%20BiteSite%2C%20I%20saw%20your%20website%20and%20I%20would%20like%20to%20know%20more%20about%20your%20restaurant%20menu%20service.";
