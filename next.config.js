aef555ff601a0adfe865f1a7e24fbbb44bc8a94c
chlew467779-new <chlew467779@gmail.com>
2026-08-29 20:49:52 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  poweredByHeader: false,
+  compress: true,
   images: {
     unoptimized: true,
     remotePatterns: [
